7ff367698adf28f16bfeb8a10848e1c773488db3
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 15:01:09 +0530

    Create UserLogin.java

---
+++ UserLogin.java
@@ -0,0 +1,10 @@
+public class UserLogin {
+    public static void main(String[] args) {
+        boolean isLoggedIn = true;
+        if (isLoggedIn) {
+            System.out.println("User is logged in.");
+        } else {
+            System.out.println("User is not logged in.");
+        }
+    }
+}
