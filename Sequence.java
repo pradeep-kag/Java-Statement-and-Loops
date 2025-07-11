a5608ac714f8bc77a76fa1e04713d4ba6a899cda
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 14:42:34 +0530

    Create Sequence.java

---
+++ Sequence.java
@@ -0,0 +1,9 @@
+public class Sequence {
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            System.out.println(i);
+            i += 5;
+        } while (i <= 50);
+    }
+}
