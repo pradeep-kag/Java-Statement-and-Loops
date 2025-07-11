d6f1f673abb27b9dd6ebcdb2fadd6dd075f44b89
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 14:57:56 +0530

    Create Adult.java

---
+++ Adult.java
@@ -0,0 +1,10 @@
+public class Adult {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Person is an adult.");
+        } else {
+            System.out.println("Person is not an adult.");
+        }
+    }
+}
