ba4b1698d788ffb7a640f31eb1affd58b4f77075
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 14:42:01 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,1 +1,10 @@
-
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = 456, sum = 0;
+        do {
+            sum += num % 10;
+            num /= 10;
+        } while (num != 0);
+        System.out.println("Sum of digits = " + sum);
+    }
+}
