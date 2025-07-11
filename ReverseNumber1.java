a442d226cc2f2d67c2203e9284ee2954069b157a
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 15:09:13 +0530

    Create ReverseNumber1.java

---
+++ ReverseNumber1.java
@@ -0,0 +1,11 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 1234, reverse = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reversed number = " + reverse);
+    }
+}
