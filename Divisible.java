739cd1a0eff1813677d5231d06f1c3ecd3850bb4
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 14:58:23 +0530

    Create Divisible.java

---
+++ Divisible.java
@@ -0,0 +1,8 @@
+public class Divisible {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num % 5 == 0) {
+            System.out.println(num + " is divisible by 5.");
+        }
+    }
+}
