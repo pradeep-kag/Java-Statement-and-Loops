c0a898178092826a703fc4785f38905d891395b6
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 15:01:34 +0530

    Create Vote.java

---
+++ Vote.java
@@ -0,0 +1,8 @@
+public class Vote {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Eligible to vote.");
+        }
+    }
+}
