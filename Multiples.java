faa2728513221f1b532dde991d83ef5477336708
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 15:07:06 +0530

    Create Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,10 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1, count = 0;
+        while (count < 5) {
+            System.out.println(3 * i);
+            i++;
+            count++;
+        }
+    }
+}
