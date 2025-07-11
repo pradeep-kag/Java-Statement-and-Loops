a6dc56783a4c60c5843a6ef97230031c01d6fce4
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 14:59:21 +0530

    Create Greater.java

---
+++ Greater.java
@@ -0,0 +1,8 @@
+public class Greater {
+    public static void main(String[] args) {
+        int num = 150;
+        if (num > 100) {
+            System.out.println(num + " is greater than 100.");
+        }
+    }
+}
