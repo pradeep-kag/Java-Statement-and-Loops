820abf920f1fe6aaca311a6328527fd69c4b727e
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 15:05:31 +0530

    Create Countdown.java

---
+++ Countdown.java
@@ -0,0 +1,9 @@
+public class Countdown {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
