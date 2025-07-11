4ffd5eaefb48bf186ed470dde6d86b972b323622
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 14:47:42 +0530

    Create CharCheck.java

---
+++ CharCheck.java
@@ -0,0 +1,16 @@
+public class CharCheck {
+    public static void main(String[] args) {
+        char ch = 'b';
+
+        if (Character.isLetter(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                System.out.println(ch + " is a vowel.");
+            } else {
+                System.out.println(ch + " is a consonant.");
+            }
+        } else {
+            System.out.println(ch + " is not a letter.");
+        }
+    }
+}
