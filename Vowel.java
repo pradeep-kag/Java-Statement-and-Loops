f2f334ff7a9b101fcd8368f0b27443d1288b1213
pradeep-kag <pradeepkag6@gmail.com>
2025-07-11 15:02:01 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,10 @@
+public class Vowel {
+    public static void main(String[] args) {
+        char ch = 'e';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch + " is a vowel.");
+        } else {
+            System.out.println(ch + " is not a vowel.");
+        }
+    }
+}
